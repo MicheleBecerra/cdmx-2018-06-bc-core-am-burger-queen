82732369bf6cadbbd8320852f8f83d2b41cfd73d
michele velazquez de la rosa becerra <michbecerra@hotmail.com>
2018-10-18 11:53:33 -0500

    Enlazando todas las rutas con los metodos

--- server/controllers/menu-controller.js
+++ server/controllers/menu-controller.js
@@ -1,12 +1,24 @@
 const comandaCtrl = {}
 
-comandaCtrl.getComanda = (req, res) => { 
-    res.json({
-      status: 'Las comandas van aquí'
-    })
-  }
+comandaCtrl.getComandas = (req, res) => {
+  res.json({
+    status: 'Las comandas van aquí'
+  })
+}
 
 comandaCtrl.createComanda = function () {
+
+}
+
+comandaCtrl.getComanda = function () {
+
+}
+
+comandaCtrl.editComanda = function () {
+
+}
+
+comandaCtrl.deleteComanda = function () {
 
 }
 
--- server/routes/menu-routes.js
+++ server/routes/menu-routes.js
@@ -2,8 +2,10 @@ const express = require('express')
 const router = express.Router()
 const menuCtrl = require('../controllers/menu-controller')
 
-router.get('/', menuCtrl.getComanda)
+router.get('/', menuCtrl.getComandas)
 router.post('/', menuCtrl.createComanda)
 router.get('/:id', menuCtrl.getComanda)
-router.put('/:id', )
+router.put('/:id', menuCtrl.editComanda)
+router.delete('/:id', menuCtrl.deleteComanda)
+
 module.exports = router

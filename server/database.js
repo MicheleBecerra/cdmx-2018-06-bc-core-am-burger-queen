491826537feaf0dcb501e86849697ac835c432f1
michele velazquez de la rosa becerra <michbecerra@hotmail.com>
2018-10-23 22:05:39 -0500

    empezando de nuevo

--- server/database.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-const URI = 'mongodb://localhost/burguer-queen'
-
-mongoose.connect(URI)
-  .then(db => console.log('DB is connected'))
-  .then(err => console.log(err))
-
-module.exports = mongoose

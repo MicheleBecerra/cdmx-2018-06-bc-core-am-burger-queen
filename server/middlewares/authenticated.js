4a01d44b6454411765313d7520cd57b23173670c
michele velazquez de la rosa becerra <michbecerra@hotmail.com>
2018-10-25 12:28:52 -0500

    hi

---
+++ server/middlewares/authenticated.js
@@ -0,0 +1,31 @@
+const jwt = require('jwt-simple')
+const moment = require('moment')
+const secret = 'clave_secreta_burguer_queen_offline';
+
+// Se usa como metodo del middleware ensure, el middleware, va recibir como paarametro req, res y next. Hasta que se ejecute next se va a ejecutar el metodo de la ruta que se esta llamando 
+exports.ensureAuth = function (req, res, next){
+    if(!req.headers.authorization){
+        return res.status(403).send({ message:'La peticion no tienen la cabecera de autenticacion'})
+    }
+
+    const token = req.headers.authorization.replace(/['"] +/g,'');
+
+    // Se va a decodificar el token usando el metodo decode y la clave secreta. El payload se introducira en un try-catch para que si existe un problema en la petición cache el error y lo muestre.
+    try{ 
+        const payload = jwt.decode(token, secret)
+
+        if(payload.exp >= moment().unix()){
+            return res.status(401).send({
+                message:'El token ha expirado'
+            })
+        }
+    } catch(ex){
+        return res.status(404).send({
+            message:'El token no es valido'
+        })
+    }
+    // Se iguala payload al req para que siempre este ligado el usuario logeado a los controladores
+    req.user = payload;
+
+    next();
+}
--- server/routes/user.js
+++ server/routes/user.js
@@ -2,10 +2,12 @@ const express = require('express')
 const UserController = require('../controllers/user')
 
 const api = express.Router()
+const md_auth = require('../middlewares/authenticated')
 
 api.get('/home', UserController.home)
-api.get('/pruebas', UserController.pruebas)
+api.get('/pruebas', md_auth.ensureAuth, UserController.pruebas)
 api.post('/register', UserController.saveUser)
 api.post('/login', UserController.loginUser)
 
-module.exports= api
+
+module.exports= api;

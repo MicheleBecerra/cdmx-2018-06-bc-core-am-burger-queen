c3ef7da027b537dea2b18008d9c20872fda4ebd5
michele velazquez de la rosa becerra <michbecerra@hotmail.com>
2018-11-07 14:54:41 -0600

    y

--- front/src/app/models/user.ts
+++ front/src/app/models/user.ts
@@ -1,5 +1,10 @@
 export class User {
     construsctor(
-
+        public _id: string,
+        public name: string,
+        public role: string,
+        public email: string,
+        public password: string,
+        public image: string
     ) {}
 }

b3d4d8965c50f7212bc8f5e438f765779717b19a
michele velazquez de la rosa becerra <michbecerra@hotmail.com>
2018-11-07 18:29:27 -0600

    y

---
+++ front/src/app/models/comanda.ts
@@ -0,0 +1,13 @@
+export class Comanda {
+    construstos(
+        public _id: string,
+        public user:string,
+        public desayuno: string,
+        public comida: string,
+        public bebida: string,
+        public total: number,
+        public file: string,
+        public created_at: string,
+
+    ) {}
+}

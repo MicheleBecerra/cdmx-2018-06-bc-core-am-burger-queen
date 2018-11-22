8d4485428beb7be74b826e7d806ca9141c880f2c
michele velazquez de la rosa becerra <michbecerra@hotmail.com>
2018-11-22 00:36:48 -0600

    Se ha agregado el componente home y se la hecho la ruta para el mismo

--- front/src/app/app.routing.ts
+++ front/src/app/app.routing.ts
@@ -5,10 +5,12 @@ import { Routes, RouterModule } from '@angular/router';
 // Importamos los componentes login y register
 import {LoginComponent} from './components/login/login.component';
 import {RegisterComponent} from './components/register/register.component';
+import { HomeComponent } from './components/home/home.component';
 
 // Array de objetos json que definen las rutas
 const appRoutes: Routes = [
-    {path: '', component: LoginComponent},
+    {path: '', component: HomeComponent},
+    {path: 'home', component: HomeComponent},
     {path: 'login', component: LoginComponent},
     {path: 'registro', component: RegisterComponent},
 ];
